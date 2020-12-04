df09b09dc913bd89d0420bef02b162b9337bd9b3
ted <ted@warpedpuppy.com>
2020-12-04 11:33:53 -0500

    remove child state

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import Parent from './Parent';
 export default class componentName extends Component {
-
-  state = {
-      returnToParent: false
-  }
   render() {
         return (<h1>hello I'm child</h1> )
   }
